49dfaf8139069b5e147ea96654fdee119d5cba5b
Muhammed Furkan GÜNGÖR <mfg263@gmail.com>
2020-03-28 17:41:53 +0300

    what if user exists and not exists

--- lib/message processing/processor.ex
+++ lib/message processing/processor.ex
@@ -5,13 +5,20 @@ defmodule LineProcessor do
       "ping\n" -> Client.send_message("pong\n", state.socket)
       "mynameis " <> name ->
         name = String.trim(name)
-        send pid, {:update_name, name}
-        Client.send_message("ok, your name is " <> name <> "\n", state.socket)
+        case UserStorage.get_user(name) do
+          nil ->
+            send pid, {:update_name, name}
+            Client.send_message("ok, your name is " <> name <> "\n", state.socket)
+          _ ->
+            Client.send_message("name already exists\n", state.socket)
+        end
       "whatismyname\n" -> Client.send_message("hellooo, you are " <> state.name <> "\n", state.socket)
       "sendmsg " <> rest ->
         [to, msg] = rest |> String.split(" ", parts: 2)
-        socket = UserStorage.get_user(to)
-        Client.send_message(msg, socket)
+        case UserStorage.get_user(to) do
+          nil -> Client.send_message("user not found\n", state.socket)
+          socket -> Client.send_message(msg, socket)
+        end
       _ -> Client.send_message("unknown command\n", state.socket)
     end
   end
